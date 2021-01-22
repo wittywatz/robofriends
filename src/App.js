fdf027b1d88e123144acc707e301e74eaac365d9
Watson <35387099+WittyWatzz@users.noreply.github.com>
2021-01-21 21:38:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import Scroll from './Scroll';
 class App extends React.Component{
 	constructor(){
 		super();
+		
 		this.state = {
 			robots: [],
 			search: ""
 		}
 	}
+	
 	componentDidMount(){
 		fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users =>{this.setState({robots: users})})
-
-
 	}
+	
 	onSearchChange = (event) => {
 		this.setState({search: event.target.value})	
 	};
@@ -27,6 +28,7 @@ class App extends React.Component{
 				robot.name.toLowerCase().includes(this.state.search.toLowerCase())
 			);
 		});
+		
 		return(
 				<div className="tc">
 					<h1 className="f1">ROBOFRIENDS</h1>
